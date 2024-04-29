4edb1a0c78fd5d269f15fe57b150f6d585d957fb
Henrique Pires <henrique.pires@extreme.digital>
2024-04-29 09:19:02 -0300

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  map: string =
+    'https://www.google.com/maps/place/Shield+Prote%C3%A7%C3%A3o+Veicular/@-18.9221783,-48.2915268,17z/data=!4m16!1m9!4m8!1m0!1m6!1m2!1s0x94a445473a7c38bd:0x8b58a5405a0ac327!2sAv.+Get%C3%BAlio+Vargas,+1328+-+Centro,+Uberl%C3%A2ndia+-+MG,+38400-434!2m2!1d-48.2889519!2d-18.9221783!3m5!1s0x94a445473a7c38bd:0x8b58a5405a0ac327!8m2!3d-18.9221783!4d-48.2889519!16s%2Fg%2F11nxpmysk0?entry=ttu';
   detalhes: any;
   value: any;
-  app: any[] = [
-    '2 via de boleto', 'Consulta', 'Benefícios', 'Fácil e prático'
-  ];
+  app: any[] = ['2 via de boleto', 'Consulta', 'Benefícios', 'Fácil e prático'];
   cards: any[] = [
     {
       titulo: 'Proteção contra furto e roubo',
@@ -40,4 +40,10 @@ export class AppComponent {
   ];
   constructor() {}
 
+  scrollToPosition(position: number) {
+    window.scrollTo({
+      top: position,
+      behavior: 'smooth',
+    });
+  }
 }
